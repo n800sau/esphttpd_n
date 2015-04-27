60621cebcc275966b44ab38c9d044ca97d3ddd68
n800sau <n800sau@gmail.com>
2015-04-27 16:15:28 +1000

    bw mask bmp

--- cam.py
+++ cam.py
@@ -7,6 +7,10 @@ import select
 
 HOST = '192.168.1.97'	  # The remote host
 PORT = 23			  # The same port as used by the server
+
+file('garbage.log','w')
+file('received.log','w')
+
 s = None
 for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
 	af, socktype, proto, canonname, sa = res
@@ -30,20 +34,16 @@ s.setblocking(1)
 s.settimeout(1)
 
 def read_byte():
-	while True:
-		b = bytearray(1)
-		n = s.recv_into(b)
-		if n > 0:
-			break
-		else:
-			select.select([s], [], [s], timeout=1)
+	b = bytearray(1)
+	s.recv_into(b)
+	file('received.log','a').write(b)
 	return b
 
 def read_dat(nlines, datawidth):
 	rs = ''
 	for i in range(nlines):
 		_read_until('DAT: ')
-		l = _read_size(datawidth)
+		l = read_size(datawidth)
 #		print len(l), i
 		rs += l
 	return rs
@@ -56,19 +56,14 @@ def _read_until(s='\r'):
 			break;
 	return rs
 
-def _read_size(size = 1):
+def read_size(size = 1):
 	i = 0
 	rs = ''
-	b = bytearray(1)
 	while True:
-		n = s.recv_into(b)
-		if n > 0:
-			rs += b
-			i += 1
-			if i >= size:
-				break
-		else:
-			time.sleep(0.00001)
+		rs += read_byte()
+		i += 1
+		if i >= size:
+			break
 	return rs
 
 def read_until(c='\r'):
@@ -88,23 +83,90 @@ def send_at(cmd):
 def send_cmu(cmd=''):
 	rs = str(send(cmd + '\r')).strip()
 	if cmd:
-		if not rs.endswith('ACK'):
-			raise Exception('Error:' + '\n'.join(rs))
+		if not (rs.endswith('ACK') or rs.endswith(':')):
+			if rs.endswith('NCK'):
+				raise Exception('Invalid command or parameters  %s' % cmd) 
+			else:
+				raise Exception('Error:' + '\n'.join(rs))
 	return rs
 
-def send_cmu_end():
-	send('\r')
+def send_cmu_stop():
+	s.sendall('\r')
+	flush()
 
-send_at('USEC')
-send_at('BAUD 19200')
-send_cmu('GV')
-print read_until('\r')
-#		for i in range(10):
-send_at('LFLASH')
-send_cmu('SF 3 3')
-f = file('data.bin','w')
-f.write(read_dat(80, 60*2))
-send_cmu()
-print 'End'
+def flush():
+	try:
+		while True:
+			b = s.recv(1)
+			print >>file('garbage.log', 'a'), b
+	except socket.timeout:
+		pass
+
+def parseData(data):
+	rs = None
+	data = str(data).lstrip()
+	if data[1] == ' ':
+		typ = data[0]
+		data = data[2:]
+		if typ == 'F':
+			rs = data[:600]
+		elif typ == 'H':
+			rs = [int(v)*100/255 for v in data.strip().split(' ')]
+		elif typ == 'S':
+			rs = [float(v) for v in data.strip().split(' ')]
+		elif typ == 'T':
+			rs = dict(zip(['mx', 'my', 'x1', 'y1', 'x2', 'y2', 'pixels', 'confidence'], [float(v) for v in data.strip().split(' ')]))
+	if rs is None:
+		raise Exception('Not parseable data (size: %d, type:%s, space:0x%2.2X): %s ...' % (len(data), data[:1], data[1], data[:10]))
+	return rs
+
+def run():
+	send_at('USEC')
+	send_at('BAUD 19200')
+	send_cmu_stop()
+	#send_cmu('GV')
+	#print read_until('\r')
+	# switch to color track mode (opposite to yuv)
+	send_cmu('CT 0')
+	send_at('LFLASH')
+	send_cmu('SF 3 3')
+	f = file('data.bin','w')
+	f.write(read_dat(80, 60*2))
+	send_cmu_stop()
+#	send_cmu('CT 0')
+#	send_cmu('ST')
+	send_cmu('SB')
+	f = file('bwdata.bin','w')
+	f.write(read_size(600))
+	send_cmu_stop()
+	send_cmu('GH 0 5')
+	hred = parseData(read_until())
+	send_cmu_stop()
+	send_cmu('GH 1 5')
+	hgreen = parseData(read_until())
+	send_cmu_stop()
+	send_cmu('GH 2 5')
+	hblue = parseData(read_until())
+	send_cmu_stop()
+
+	f = file('gh.html','w')
+
+	binsize = 256/len(hred)
+
+	print >>f, '<table><tr>'
+	for t,r,g,b in zip([('%s-%s' % (i, i+binsize-1)) for i in range(0, 256, binsize)], hred, hgreen, hblue):
+		print >>f, '<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>' % (t, r, g, b)
+	print >>f, '</tr></table>'
+
+	f.close()
+
+	send_cmu('ST 16 31 16 31 16 31')
+
+	print 'End'
+
+try:
+	run()
+except Exception, e:
+	print >>file('error.log', 'w'), e
 
 s.close()

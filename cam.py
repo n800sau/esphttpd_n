ecfcc9879816c45f19f1c00bcd7def3c310ee1b7
n800sau <n800sau@gmail.com>
2015-04-28 11:56:43 +1000

    some more commands added

--- cam.py
+++ cam.py
@@ -4,6 +4,8 @@ import socket
 import sys
 import time
 import select
+import json
+import traceback
 
 HOST = '192.168.1.97'	  # The remote host
 PORT = 23			  # The same port as used by the server
@@ -31,7 +33,7 @@ if s is None:
 	sys.exit(1)
 
 s.setblocking(1)
-s.settimeout(1)
+s.settimeout(1.5)
 
 def read_byte():
 	b = bytearray(1)
@@ -117,28 +119,25 @@ def parseData(data):
 		elif typ == 'T':
 			rs = dict(zip(['mx', 'my', 'x1', 'y1', 'x2', 'y2', 'pixels', 'confidence'], [float(v) for v in data.strip().split(' ')]))
 	if rs is None:
-		raise Exception('Not parseable data (size: %d, type:%s, space:0x%2.2X): %s ...' % (len(data), data[:1], data[1], data[:10]))
+		raise Exception('Not parseable data (size: %d, type:%s, space:0x%2.2X): %s ...' % (len(data), data[:1], ord(data[1]) if isinstance(data[1], basestring) else data[1], data[:10]))
 	return rs
 
-def run():
-	send_at('USEC')
-	send_at('BAUD 19200')
-	send_cmu_stop()
-	#send_cmu('GV')
-	#print read_until('\r')
-	# switch to color track mode (opposite to yuv)
-	send_cmu('CT 0')
-	send_at('LFLASH')
+def file_data_bin():
+#	send_at('LFLASH')
 	send_cmu('SF 3 3')
 	f = file('data.bin','w')
 	f.write(read_dat(80, 60*2))
 	send_cmu_stop()
-#	send_cmu('CT 0')
-#	send_cmu('ST')
+
+def file_bwdata_bin():
 	send_cmu('SB')
 	f = file('bwdata.bin','w')
 	f.write(read_size(600))
 	send_cmu_stop()
+
+def file_gh_html():
+	send_cmu('CT 0')
+
 	send_cmu('GH 0 5')
 	hred = parseData(read_until())
 	send_cmu_stop()
@@ -149,24 +148,91 @@ def run():
 	hblue = parseData(read_until())
 	send_cmu_stop()
 
+	send_cmu('CT 1')
+
+	send_cmu('GH 0 5')
+	hV = parseData(read_until())
+	send_cmu_stop()
+	send_cmu('GH 1 5')
+	hY = parseData(read_until())
+	send_cmu_stop()
+	send_cmu('GH 2 5')
+	hU = parseData(read_until())
+	send_cmu_stop()
+
 	f = file('gh.html','w')
 
 	binsize = 256/len(hred)
 
 	print >>f, '<table><tr>'
-	for t,r,g,b in zip([('%s-%s' % (i, i+binsize-1)) for i in range(0, 256, binsize)], hred, hgreen, hblue):
-		print >>f, '<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>' % (t, r, g, b)
+	print >>f, '<td></td><td>R</td><td>G</td><td>B</td><td>V</td><td>U</td><td>Y</td></tr><tr>'
+	for t,r,g,b, v, u, y in zip([('%s-%s' % (i, i+binsize-1)) for i in range(0, 256, binsize)],
+			hred, hgreen, hblue, hV, hU, hY):
+		print >>f, '<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>' % (t, r, g, b, v, u, y)
 	print >>f, '</tr></table>'
 
 	f.close()
 
-	send_cmu('ST 16 31 16 31 16 31')
+def show_gv():
+	send_cmu('GV')
+	print read_until('\r')
+
+def file_twdata():
+	send_cmu('TW 5 5 5')
+	f = file('twdata.json','w')
+	json.dump(parseData(read_until()), f)
+
+def file_tcdata():
+	send_cmu('TC 96 100 0 255 0 255')
+	f = file('tcdata.json','w')
+	json.dump(parseData(read_until()), f)
+
+
+def run():
+	send_at('USEC')
+#	send_at('BAUD 19200')
+	send_at('BAUD 38400')
+#	send_at('BAUD 115200')
+	send_cmu_stop()
+#	send_cmu('BM 38400')
+#	send_cmu('BM 115200')
+#	send_at('BAUD 38400')
+#	show_gv()
+#	send_cmu_stop()
+	# switch to poll mode
+	send_cmu('PM 1')
+	# turn off line mode
+	send_cmu('LM 0')
+
+#	file_gh_html()
+	# switch to color track mode (opposite to yuv)
+	send_cmu('CT 1')
+	send_cmu('HT 0')
+
+# reset colors
+#	send_cmu('ST')
+
+	send_cmu('ST 64 95 0 0 0 0')
+
+	send_cmu_stop()
+
+	file_tcdata()
+
+#	file_twdata()
+
+	file_data_bin()
+
+	send_cmu_stop()
+
+	file_bwdata_bin()
+
+	send_cmu_stop()
 
 	print 'End'
 
 try:
 	run()
-except Exception, e:
-	print >>file('error.log', 'w'), e
+except Exception:
+	traceback.print_exc(10, file('error.log', 'w'))
 
 s.close()
